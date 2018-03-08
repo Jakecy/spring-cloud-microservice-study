3a1a2dafbfb17e84939fe15b5acc18d05273a93e
chihaojie <2499384823@qq.com>
2018-03-09 07:21:52 +0800

    添加一个新服务-spring cloud config server

---
+++ microservice-config-server/src/main/java/com/ConfigApplication.java
@@ -0,0 +1,15 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+        System.out.println(".....Cofig Server starts successfully ......");
+    }
+}
